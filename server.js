4e06b357fb666d7382a103ef2ead2c3c58a32db7
VictorKholyavo <victor.kholyavo@gmail.com>
2019-03-04 20:20:11 +0300

    dataset-B (final)

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let path = require('path');
 
 let app = express();
 let db;
+let dbDynamic;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../')));
@@ -18,6 +19,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', function (req, res) {
 	res.send('Hello API');
 })
+
+//FILMS for datasetA//
 
 app.get('/films', function (req, res) {
 	db.collection('films').find().toArray(function (err, docs) {
@@ -47,11 +50,6 @@ app.get('/films/:id', function (req, res) {
 })
 
 app.post('/films', function (req, res) {
-	let artist = {
-		newid: req.body._id,
-		name: req.body.name
-	};
-
 	db.collection('films').insertOne(req.body, function (err, result) {
 		if (err) {
 			console.log(err);
@@ -62,12 +60,13 @@ app.post('/films', function (req, res) {
 })
 
 app.put('/films/:id', function (req, res) {
+	console.log('films');
 	db.collection('films').updateOne(
 		{ _id: ObjectID(req.params.id)},
 		{$set: {rank: req.body.rank, title: req.body.title, year: req.body.year, votes: req.body.votes, rating: req.body.rating}},
 		{
 			upsert: false,
-			multi: false
+			multi: true
 		},
 		function (err) {
 			if (err) {
@@ -91,12 +90,76 @@ app.delete('/films/:id', function (req, res) {
 	)
 })
 
-MongoClient.connect('mongodb://localhost:27017/myapi', { useNewUrlParser: true }, function (err, database) {
+//USERS for datasetB//
 
+app.get('/users', function (req, res) {
+	let order = req.query.sort ? ["name", req.query.sort.name ] : [];
+	dbDynamic.collection('users').find().sort(order).toArray(function (err, docs) {
+
+		let start = req.query.start;
+		let count = req.query.count;
+		let arr = [];
+
+		for (var i = 0; i < docs.length; i++){
+				docs[i].id = docs[i]._id;
+				delete docs[i]._id;
+		}
+
+		for (let i = start; i <= +start + +count; i++) {
+			arr.push(docs[i]);
+		}
+		res.send({"pos": start, "data": arr, "total_count": 778});
+
+		if (err) {
+			console.log(err);
+			return res.sendStatus(500)
+		}
+	})
+})
+
+app.get('/users/:id', function (req, res) {
+	dbDynamic.collection('users').findOne(
+		{ _id: req.params.id },
+		function (err, docs) {
+			docs.id = docs._id;
+			delete docs._id
+			res.send(docs)
+		}
+	)
+})
+
+app.put('/users/:id', function (req, res) {
+	dbDynamic.collection('users').updateOne(
+		{ _id: req.params.id},
+		{$set: {name: req.body.name, age: req.body.age, gender: req.body.gender, company: req.body.company, favoriteFruit: req.body.favoriteFruit}},
+		{
+			upsert: false,
+			multi: true
+		},
+		function (err, result) {
+			if (err) return res.send({ status:"error" });
+			res.send({});
+		})
+})
+
+app.delete('/users/:id', function (req, res) {
+	dbDynamic.collection('users').deleteOne(
+		{ _id: ObjectID(req.params.id)},
+		function (err) {
+			if (err) {
+				return res.send({ status:"error" });
+			}
+			res.send({});
+		}
+	)
+})
+
+MongoClient.connect('mongodb://localhost:27017/myapi', { useNewUrlParser: true }, function (err, database) {
 	if (err) {
 		return console.log(err);
 	}
 	db = database.db('films');
+	dbDynamic = database.db('users');
 	app.listen(3012, function () {
 		console.log('API app started');
 	})
